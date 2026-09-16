e773cea687370963e3b5b2e40c3027bb17503fa6
JaCharer <twoj.email@example.com>
2026-09-16 09:33:07 +0200

    UDP and timeouts

--- src/nmea_udp/udp_actisense.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-void udp_actisense_load_config(void);
-bool udp_actisense_begin(void);
-void udp_actisense_process(void);
-bool udp_actisense_connected(void);
--- src/nmea_udp/udp_config_screen.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <lvgl.h>
-
-void udp_config_screen_attach(lv_obj_t *settings_screen);
